e8667348e37ccbdb43f63e6580202990d69e48c3
jordandbs <desbordes.jordan@educ-valadon-limoges.fr>
2024-09-25 16:58:31 +0200

    test

---
+++ lib/DataBase/DatabaseClient.dart
@@ -0,0 +1,50 @@
+import 'package:path/path.dart';
+import 'package:sqflite/sqflite.dart';
+
+class DatabaseClient {
+  static final DatabaseClient _instance = DatabaseClient._internal();
+  static Database? _database;
+
+  DatabaseClient._internal();
+
+  factory DatabaseClient() {
+    return _instance;
+  }
+
+  Future<Database> get database async {
+    if (_database != null) return _database!;
+
+    _database = await _initDatabase();
+    return _database!;
+  }
+
+  Future<Database> _initDatabase() async {
+    String dbPath = await getDatabasesPath();
+    String path = join(dbPath, 'bibliotheque.db');
+
+    return await openDatabase(
+      path,
+      version: 1,
+      onCreate: _onCreate,
+    );
+  }
+
+  Future<void> _onCreate(Database db, int version) async {
+    // Création de la table Auteur
+    await db.execute('''
+      CREATE TABLE AUTEUR(
+        idAuteur INTEGER PRIMARY KEY AUTOINCREMENT,
+        nomAuteur TEXT NOT NULL
+      )
+    ''');
+
+    // Création de la table Livre
+    await db.execute('''
+      CREATE TABLE LIVRE(
+        idLivre INTEGER PRIMARY KEY AUTOINCREMENT,
+        nomLivre TEXT NOT NULL,
+        idAuteur INTEGER,
+        FOREIGN KEY(idAuteur) REFERENCES AUTEUR(idAuteur)
+      )
+    ''');
+  }
